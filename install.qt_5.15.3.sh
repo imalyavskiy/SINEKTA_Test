7cdbcbc177c8205a7755ce304687e59e661f5ea0
Ilya Malyavskiy <ilya.malyavskiy@gmail.com>
2025-02-26 22:57:26 +0700

    qt install/remove scripts updated

---
+++ install.qt_5.15.3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt install -y qt5-qmake 
+
+sudo apt install -y qt5-doc 
+sudo apt install -y qt5-doc-html
+
+sudo apt install -y qtbase5-dev 
+sudo apt install -y qtbase5-examples 
+sudo apt install -y qtbase5-doc-html
+
+sudo apt install -y qtquickcontrols2-5-examples
+
+sudo apt install -y qtdeclarative5-dev
+
+sudo apt install -y qml
+sudo apt install -y qml-module-qtquick-controls2
+sudo apt install -y qml-module-qtquick-controls
+sudo apt install -y qml-module-qtquick-dialogs
+
+sudo apt install -y qtcreator 
---
+++ remove.qt_5.15.3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+sudo apt remove -y qt5-qmake 
+
+sudo apt remove -y qt5-doc 
+sudo apt remove -y qt5-doc-html
+
+sudo apt remove -y qtbase5-dev 
+sudo apt remove -y qtbase5-examples 
+sudo apt remove -y qtbase5-doc-html
+
+sudo apt remove -y qtquickcontrols2-5-examples
+
+sudo apt remove -y qtdeclarative5-dev
+
+sudo apt remove -y qml
+sudo apt remove -y qml-module-qtquick-controls2
+sudo apt remove -y qml-module-qtquick-controls
+sudo apt remove -y qml-module-qtquick-dialogs
+
+sudo apt remove -y qtcreator 
